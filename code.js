15982e3c7612916aca97c96191b17f69b3d2a848
DancingPotato <herobrinesayeh@gmail.com>
2023-03-07 03:46:50 +0200

    Finish playRound()

--- code.js
+++ code.js
@@ -9,14 +9,112 @@ function getComputerOutput() {
     }
     if(randomChoice == 1)
     {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     }
     else if(randomChoice == 2)
     {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }
     else{
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
     }
     return computerChoice;
 }
+
+function getPlayerInput()
+{
+    let playerInput
+    
+    return playerInput;
+}
+
+function win()
+{
+    return "You win! " + playerChoice + " beats " + computerChoice;
+}
+
+function lose()
+{
+    return "You lose.. " + computerChoice + " beats " + playerChoice;
+}
+
+function checkResult(bot, player)
+{
+    if(bot == player)
+    {
+        return "Draw!";
+    }
+
+    //bot
+    switch (bot) {
+        //bot gets rock
+        case "rock":
+            //player
+           switch (player) {
+                case "paper":
+                    return win();
+                    break;
+           
+                case "scissors":
+                    return lose();
+                    break;
+                }
+            break;
+           
+        //bot gets scissors
+        case "scissors":
+            switch (player) {
+                case "rock":
+                    return win();
+                    break;
+               
+                case "paper":
+                    return lose();
+                    break;
+               }
+            break;
+
+        //bot gets paper
+        case "paper":
+            switch (player) {
+                case "scissors":
+                    return win();
+                    break;
+               
+                case "rock":
+                    return lose();
+                    break;
+               }
+            break;
+        
+    }
+}
+
+function playRound()
+{
+    //player input
+    playerChoice = prompt("Rock, Paper, or Scissors?");
+    playerChoice = playerChoice.toLowerCase();
+
+    //player input error check
+    if(!(playerChoice == "rock" || playerChoice == "scissors" || playerChoice == "paper"))
+    {
+        alert("Error! Input either rock, paper, or scissors!");
+        playRound();
+    }
+    else{
+        //bot output
+    computerChoice = getComputerOutput();
+
+    //result
+    console.log(checkResult(computerChoice, playerChoice));
+    }
+    
+}
+
+let playerChoice;
+let computerChoice;
+
+playRound();
+
+

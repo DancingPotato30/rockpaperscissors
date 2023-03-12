cf46c17671c32fd1a96d0a6d999aa74dd26a9bfc
DancingPotato <herobrinesayeh@gmail.com>
2023-03-12 05:50:31 +0200

    Finish basic code

--- code.js
+++ code.js
@@ -30,11 +30,15 @@ function getPlayerInput()
 
 function win()
 {
+    playerPoints++
+    playerScore.textContent = `${playerPoints}`;
     return "You win! " + playerChoice + " beats " + computerChoice;
 }
 
 function lose()
 {
+    botPoints++
+    computerScore.textContent = `${botPoints}`;
     return "You lose.. " + computerChoice + " beats " + playerChoice;
 }
 
@@ -92,37 +96,101 @@ function checkResult(bot, player)
 
 function playRound()
 {
-    //player input
-    playerChoice = prompt("Rock, Paper, or Scissors?");
-    playerChoice = playerChoice.toLowerCase();
-
-    //player input error check
-    if(!(playerChoice == "rock" || playerChoice == "scissors" || playerChoice == "paper"))
-    {
-        alert("Error! Input either rock, paper, or scissors!");
-        playRound();
-    }
-    else{
-        //bot output
+    newRound = false;
+    //bot output
     computerChoice = getComputerOutput();
 
     //result
-    console.log(checkResult(computerChoice, playerChoice));
+    finalResult.textContent = checkResult(computerChoice, playerChoice);
+
+    if (botPoints == 5) {
+        gameEnd("bot");
     }
+    else if(playerPoints == 5) {
+        gameEnd("player");
+    }
+    
     
 }
 
-function game()
-{
-    replay = prompt("How many rounds?");
-    for (let i = 0; i < replay; i++) {
-        playRound();
-        
+function gameEnd(winner) {
+    
+
+    const restart = document.createElement('button');
+
+    if (winner == "bot") {
+        finalResult.textContent = "Aww.. The bot wins this time!";
     }
+    else if (winner == "player") {
+        finalResult.textContent = "Congratulations! You won!!";
+    }
+    newRound = true;
+    finalContainer.appendChild(replayBtn);
+
+    rock.style.display = 'none';
+    scissors.style.display = 'none';
+    paper.style.display = 'none';
+    
+
+}
+
+function restart() {
+    botPoints = 0;
+    playerPoints = 0;
+    playerScore.textContent = `${playerPoints}`;
+    computerScore.textContent = `${botPoints}`;
+    finalResult.textContent = "";
+    console.log("Restarted");
+    finalContainer.removeChild(replayBtn);
+
+    rock.style.display = 'block';
+    paper.style.display = 'block';
+    scissors.style.display = 'block';
+    
 }
 
 let playerChoice;
 let computerChoice;
-let replay;
+let botPoints = 0;
+let playerPoints = 0;
+let newRound;
 
-game();
+const finalContainer = document.querySelector(".containerForFinal");
+const results = document.querySelector(".results");
+const replayBtn = document.createElement("button");
+const buttons = document.querySelectorAll(".playBtn");
+const finalResult = document.querySelector(".final");
+const computerScore = document.querySelector(".computerScore");
+const playerScore = document.querySelector(".playerScore");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+replayBtn.textContent = "Restart";
+replayBtn.addEventListener('click', (e) => {
+    restart();
+});
+replayBtn.className = "replayBtn";
+playerScore.textContent = `${playerPoints}`;
+computerScore.textContent = `${botPoints}`;
+
+
+rock.addEventListener('click', (e) => {
+    playerChoice = "rock";
+        playRound(playerChoice);
+
+});
+
+paper.addEventListener('click', (e) => {
+    playerChoice = "paper";
+
+        playRound(playerChoice);
+    
+});
+
+scissors.addEventListener('click', (e) => {
+    playerChoice = "scissors";
+        playRound(playerChoice);
+
+});
+
+
